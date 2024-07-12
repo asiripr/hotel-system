a7670fb25f08a1c3837150a3ab774e6ed2a2a8c9
asiripr <asiripramodaya2000@gmail.com>
2024-07-12 10:55:37 +0530

    varified admin

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -8,5 +8,14 @@
 </head>
 <body>
     <h1>HELLO ADMIN!</h1>
+    <form method="POST" action="{{ route('logout') }}">
+        @csrf
+
+        <x-responsive-nav-link :href="route('logout')"
+                onclick="event.preventDefault();
+                            this.closest('form').submit();">
+            {{ __('Log Out') }}
+        </x-responsive-nav-link>
+    </form>
 </body>
 </html>
