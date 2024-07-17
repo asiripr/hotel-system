30e6a3136ba12f25cea92ddc12f3e1689a8da8a7
asiripr <asiripramodaya2000@gmail.com>
2024-07-17 07:26:41 +0530

    add foods

--- resources/views/food.blade.php
+++ resources/views/food.blade.php
@@ -12,6 +12,7 @@
     <div class="menu-item-carousel">
         <div class="col-lg-12">
             <div class="owl-menu-item owl-carousel">
+                
                 <div class="item">
                     <div class='card card1'>
                         <div class="price"><h6>$14</h6></div>
@@ -24,66 +25,7 @@
                         </div>
                     </div>
                 </div>
-                <div class="item">
-                    <div class='card card2'>
-                        <div class="price"><h6>$22</h6></div>
-                        <div class='info'>
-                          <h1 class='title'>Klassy Pancake</h1>
-                          <p class='description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sedii do eiusmod teme.</p>
-                          <div class="main-text-button">
-                              <div class="scroll-to-section"><a href="#reservation">Make Reservation <i class="fa fa-angle-down"></i></a></div>
-                          </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="item">
-                    <div class='card card3'>
-                        <div class="price"><h6>$18</h6></div>
-                        <div class='info'>
-                          <h1 class='title'>Tall Klassy Bread</h1>
-                          <p class='description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sedii do eiusmod teme.</p>
-                          <div class="main-text-button">
-                              <div class="scroll-to-section"><a href="#reservation">Make Reservation <i class="fa fa-angle-down"></i></a></div>
-                          </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="item">
-                    <div class='card card4'>
-                        <div class="price"><h6>$10</h6></div>
-                        <div class='info'>
-                          <h1 class='title'>Blueberry CheeseCake</h1>
-                          <p class='description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sedii do eiusmod teme.</p>
-                          <div class="main-text-button">
-                              <div class="scroll-to-section"><a href="#reservation">Make Reservation <i class="fa fa-angle-down"></i></a></div>
-                          </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="item">
-                    <div class='card card5'>
-                        <div class="price"><h6>$8.50</h6></div>
-                        <div class='info'>
-                          <h1 class='title'>Klassy Cup Cake</h1>
-                          <p class='description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sedii do eiusmod teme.</p>
-                          <div class="main-text-button">
-                              <div class="scroll-to-section"><a href="#reservation">Make Reservation <i class="fa fa-angle-down"></i></a></div>
-                          </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="item">
-                    <div class='card card3'>
-                        <div class="price"><h6>$7.25</h6></div>
-                        <div class='info'>
-                          <h1 class='title'>Klassic Cake</h1>
-                          <p class='description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sedii do eiusmod teme.</p>
-                          <div class="main-text-button">
-                              <div class="scroll-to-section"><a href="#reservation">Make Reservation <i class="fa fa-angle-down"></i></a></div>
-                          </div>
-                        </div>
-                    </div>
-                </div>
+
             </div>
         </div>
     </div>
